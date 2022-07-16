49eda3f59a397c4fa23cc2d039c89c6d6232b052
Onion <87307561+IAmTheOnion@users.noreply.github.com>
2022-07-16 20:43:06 +0200

    Create statement_reverse.java

---
+++ statement_reverse.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Scanner;
+public class statement_reverse
+{
+	public static void main(String a[]) throws Exception
+	{
+	String strarr[];    //array which will hold splitted strings of the statement
+        String st, result = "";      //contains user input statement 
+       BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+       System.out.println("Enter the Statement:"); 
+         st=br.readLine();
+         String split[] = st.split(" ", 0);
+         for (String x : split) {
+             result = x + " " + result;
+         }
+System.out.println("The reverse is:");
+
+      
+   /*write down your logic here*/ 
+    System.out.println(result);
+ 	 
+        } 
+	 
+}  
+   
+ 
+
+
